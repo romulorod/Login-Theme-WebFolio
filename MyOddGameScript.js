cfcff29ae4860623f79a2ce8476c7d8985d8320c
romulorod <80011318+romulorod@users.noreply.github.com>
2021-05-22 22:23:44 -0300

    Some major changes on MyOddGameScript

--- MyOddGameScript.js
+++ MyOddGameScript.js
@@ -1,21 +1,64 @@
+// Variáveis Simples - NAO ATUALIZA ESSA BOSTA
 
-let valor = 0;
+let valor = 0; // Valor escolhido pelo usuário pelos botões +/-
+let escolheu = ''; // Valor da escolha entre par, impar e aleatório
+
+// Variáveis DOM
+    //Botão Menos
+const botaoMenos = document.querySelector('.menos'); // Atribuir classe à variável
+botaoMenos.addEventListener('click', menos); // executa o click - ação virgula função
+
+    //Botão Mais
+const botaoMais = document.querySelector('.mais'); // Atribuir classe à variável
+botaoMais.addEventListener('click', mais);
+
+    // Impar ou Par ?
+let escolheuPar = document.querySelector('.par'); // Atribuir classe à variável
+let escolheuImpar = document.querySelector('.impar'); // Atribuir classe à variável
+let escolheuRand = document.querySelector('.aleatorio'); // Atribuir classe à variável
+
+    //Botão Jogar
+let jogar = document.querySelector('.jogar'); // Atribuir classe à variável
+jogar.addEventListener('click', function () { // Ao clicar em jogar, seta todos os botões para cor padrão
+    escolheuPar.style.backgroundColor = "#3c7e6f";
+    escolheuImpar.style.backgroundColor = "#3c7e6f";
+    escolheuRand.style.backgroundColor = "#3c7e6f";
+});
+
+    //Definir usuaro/máquina baseado na classe do HTML
+const usuario = document.querySelector('.usuario'); // Atribuir classe à variável
+const maquina = document.querySelector('.maquina'); // Atribuir classe à variável
+
+    //Definir os botões do resultado
+let botao = document.createElement('button'); // Criar elemento button
+botao.classList.add('botao'); // Adicionar classe
+let botao2 = document.createElement('button'); //Criar elemento button
+botao2.classList.add('botao'); // Adicionar classe
+usuario.classList.add('usuario'); // Adicionar classe
+usuario.style.fontSize = "large"; //Estilização via JS
+maquina.style.fontSize = "large"; //Estilização via JS
+const resultado = document.querySelector('.resultado'); // Atribuir classe à variável
+const emojiFeliz = document.createElement('p');
+const emojiTriste = document.createElement('p2')
+//Funções
+    //Função que gera numero Randômico
 function getRandom(min, max) {
     let rand = Math.floor(Math.random() * (max - min) + min);
     return rand;
 }
 let random = getRandom(0, 6);
 
+    //Função Principal que faz o teste e insere o resultado no HTML
 let somar = function (valorUsuario, parOuImpar) {
 
     let soma = valorUsuario + random;
     const resultadoTexto = `Resultado: ${soma} <br>`;
-    soma % 2 == 0 && parOuImpar == 'par' ? resultado.innerHTML = `${resultadoTexto} Parabéns, ${soma} é par e você acertou!` :
-        soma % 2 !== 0 && parOuImpar == 'par' ? resultado.innerHTML = `${resultadoTexto} ${soma} É ímpar e você escolheu par, perdeu.` :
-            soma % 2 == 0 && parOuImpar == 'impar' ? resultado.innerHTML = `${resultadoTexto} Uma pena, ${soma} é par e você escolheu impar, perdeu.` :
-                soma % 2 !== 0 && parOuImpar == 'impar' ? resultado.innerHTML = `${resultadoTexto} ${soma} É ímpar e você acertou, parabéns!` : console.log('deu erro');
+    soma % 2 == 0 && parOuImpar == 'par' ? resultado.innerHTML = `${resultadoTexto} Parabéns, ${soma} é par e você acertou! <br> ` +`${emojiFeliz.innerHTML = '&#128516'}` :
+        soma % 2 !== 0 && parOuImpar == 'par' ? resultado.innerHTML = `${resultadoTexto} ${soma} É ímpar e você escolheu par, perdeu.<br>`+`${emojiTriste.innerHTML = '&#128557'}` :
+            soma % 2 == 0 && parOuImpar == 'impar' ? resultado.innerHTML = `${resultadoTexto} Uma pena, ${soma} é par e você escolheu impar, perdeu.<br>`+`${emojiTriste.innerHTML = '&#128557'}` :
+                soma % 2 !== 0 && parOuImpar == 'impar' ? resultado.innerHTML = `${resultadoTexto} ${soma} É ímpar e você acertou, parabéns!  <br> ` +`${emojiFeliz.innerHTML = '&#128516'}` : console.log('deu erro');
 }
-
+    //Função para o botão de menos
 function menos() {
     document.getElementsByClassName('.numEscolhido'); // pega a classe
     const numEscolhidoMenos = document.querySelector('.numEscolhido'); // joga a classe numa variavel
@@ -26,9 +69,8 @@ function menos() {
         numEscolhidoMenos.innerHTML = valor; // insere na página o decremento
     }
 }
-const botaoMenos = document.querySelector('.menos'); // joga a classe na variavel
-botaoMenos.addEventListener('click', menos); // executa o click - ação virgula função
 
+    //Função para o botão de mais
 function mais() {
     document.getElementsByClassName('.numEscolhido');
     const numEscolhidoMais = document.querySelector('.numEscolhido');
@@ -39,17 +81,7 @@ function mais() {
         alert('O número não pode ser maior que cinco');
     }
 }
-
-const botaoMais = document.querySelector('.mais');
-botaoMais.addEventListener('click', mais);
-
-
-let escolheu = '';
-let escolheuPar = document.querySelector('.par');
-let escolheuImpar = document.querySelector('.impar');
-let escolheuRand = document.querySelector('.aleatorio');
-
-
+    //Função que verifica se alguma opção foi escolhida antes de iniciar
 function verificaPar() {
     if (escolheu !== '') {
     } else {
@@ -57,8 +89,8 @@ function verificaPar() {
         return false;
     }
 }
-
-function impapa() {
+    //Função que define o botão clica, muda cor de fundo e modifica o valor da variável entre par ou impar
+function impaPar() {
 
     escolheuPar.addEventListener('click', function () {
         escolheu = 'par';
@@ -81,30 +113,15 @@ function impapa() {
         else if (x >= 3 && x <= 5) { escolheu = 'impar' };
     })
 }
-
-let jogar = document.querySelector('.jogar');
-jogar.addEventListener('click', function () {
-    escolheuPar.style.backgroundColor = "#3c7e6f";
-    escolheuImpar.style.backgroundColor = "#3c7e6f";
-    escolheuRand.style.backgroundColor = "#3c7e6f";
-});
-
+    //Função que zera o valor, para que o usuário precise escolher novamente entre par ou impar
 function limpaValor() {
     escolheu = '';
 }
 
-impapa();
-const usuario = document.querySelector('.usuario');
-const maquina = document.querySelector('.maquina');
-let botao = document.createElement('button');
-botao.classList.add('botao');
-let botao2 = document.createElement('button');
-botao2.classList.add('botao');
-usuario.classList.add('usuario');
-usuario.style.fontSize = "large";
-maquina.style.fontSize = "large";
-const resultado = document.querySelector('.resultado');
+impaPar();
 
+
+    //Função que insere no HTML através do JS as informações do resultado do jogo.
 function jogaNoHtml() {
 
     usuario.innerHTML = `Seu número foi: <br>${valor}<br> Sua escolha foi:<br>`
@@ -123,7 +140,7 @@ function jogaNoHtml() {
     }
     maquina.appendChild(botao2);
 }
-
+    //Função geral que recebe o click no botão Jogar e executa as funções cadeadamente para que tudo performe dentro do esperado.
 jogar.addEventListener('click', function () {
     if (verificaPar()==false) return;
     somar(valor, escolheu);
